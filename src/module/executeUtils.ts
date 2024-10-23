5f25de6b30f9695b2323ede4df2aafd955181f3e
carry0987 <carry0987@users.noreply.github.com>
2024-10-23 16:54:08 +0800

    Update debounce method

--- src/module/executeUtils.ts
+++ src/module/executeUtils.ts
@@ -1,10 +1,11 @@
-import { ThrottleOptions } from '@/interface/interfaces';
+import { ThrottleOptions, DebounceOptions } from '@/interface/interfaces';
 
 /**
  * Throttle a given function
  *
  * @param fn Function to be called
  * @param wait Throttle timeout in milliseconds
+ * @param options Throttle options
  *
  * @returns Throttled function
  */
@@ -50,23 +51,50 @@ export function throttle(
  * Creates a debounced function that delays the invocation of the provided function
  * until after the specified wait time has elapsed since the last time it was called.
  *
- * @param func - The original function to debounce.
- * @param waitFor - The number of milliseconds to delay the function call.
+ * @param fn - The original function to debounce.
+ * @param wait - The number of milliseconds to delay the function call.
+ * @param options - Debounce options.
  *
  * @returns A debounced function that returns a Promise resolving to the result of the original function.
  */
-export function debounce<F extends (...args: any[]) => any>(func: F, waitFor: number) {
+export function debounce<F extends (...args: any[]) => any>(
+    fn: F,
+    wait: number,
+    options: DebounceOptions = { leading: false, trailing: true }
+) {
+    const { leading = false, trailing = true, maxWait } = options;
+
     let timeoutId: ReturnType<typeof setTimeout> | undefined;
+    let lastInvokeTime = 0;
+    let result: ReturnType<F>;
+
+    const invokeFn = (args: Parameters<F>): ReturnType<F> => {
+        lastInvokeTime = Date.now();
+        result = fn(...args);
+
+        return result;
+    };
 
     return (...args: Parameters<F>): Promise<ReturnType<F>> =>
         new Promise((resolve) => {
+            const currentTime = Date.now();
+            const elapsed = currentTime - lastInvokeTime;
+
             if (timeoutId !== undefined) {
                 clearTimeout(timeoutId);
             }
 
+            if (maxWait !== undefined && elapsed >= maxWait) {
+                resolve(invokeFn(args));
+            } else if (leading && elapsed > wait) {
+                resolve(invokeFn(args));
+            }
+
             timeoutId = setTimeout(() => {
-                resolve(func(...args));
+                if (trailing && !leading) {
+                    resolve(invokeFn(args));
+                }
                 timeoutId = undefined; // Clear timeout after it's been executed
-            }, waitFor);
+            }, wait);
         });
 }
